49db5ee9cfa364a84636609df2872d1fc25dbca2
jonathan_fambro <startek591@gmail.com>
2025-03-08 19:59:46 -0500

    Make an assertion

--- angular_application_with_cypress/cypress/e2e/spec.cy.ts
+++ angular_application_with_cypress/cypress/e2e/spec.cy.ts
@@ -3,5 +3,9 @@ describe('My First Test', () => {
     cy.visit('https://example.cypress.io');
 
     cy.contains('type').click();
+
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions');
   });
 });
