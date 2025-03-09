3667e38b4a87a70ee10e5f46684fc764379952ba
jonathan_fambro <startek591@gmail.com>
2025-03-08 19:48:52 -0500

    Visit a page

--- angular_application_with_cypress/cypress/e2e/spec.cy.ts
+++ angular_application_with_cypress/cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
 describe('My First Test', () => {
-  it('Does not do much!', () => {
-    expect(true).to.equal(true);
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('https://example.cypress.io');
   });
 });
