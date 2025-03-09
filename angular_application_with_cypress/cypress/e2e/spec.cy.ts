bb189ea844d2ef89dd0fd19bdbe91e40376974f6
jonathan_fambro <startek591@gmail.com>
2025-03-08 20:44:30 -0500

    Adding more commands and assertions

--- angular_application_with_cypress/cypress/e2e/spec.cy.ts
+++ angular_application_with_cypress/cypress/e2e/spec.cy.ts
@@ -7,5 +7,11 @@ describe('My First Test', () => {
     // Should be on a new URL which
     // includes '/commands/actions'
     cy.url().should('include', '/commands/actions');
+
+    // Get an input, type into it
+    cy.get('.action-email').type('fake@email.com');
+
+    // Verify that the value has been updated
+    cy.get('.action-email').should('have.value', 'fake@email.com');
   });
 });
